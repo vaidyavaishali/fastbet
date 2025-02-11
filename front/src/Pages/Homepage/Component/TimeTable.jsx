4c19ba443c1ffcaec96b5fd157f9bf1ad239cceb
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2025-02-11 23:58:55 +0530

    first commit

--- front/src/Pages/Homepage/Component/TimeTable.jsx
+++ front/src/Pages/Homepage/Component/TimeTable.jsx
@@ -66,7 +66,7 @@ const Link = styled.a`
 
 const TimeTable = () => {
   const [data, setData] = useState(null);
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -86,7 +86,8 @@ const TimeTable = () => {
     return <div>Loading...</div>;
   }
 
-  const { chartData } = data.scrapedData;
+  const { scrapedData = {} } = data;
+  const { chartData = [] } = scrapedData;
 
   return (
     <MainContainer id="time-table">
